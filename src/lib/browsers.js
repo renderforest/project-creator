bd288408e2dae8a335f0793b916c88a0aca122ab
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-06-21 15:33:45 +0400

    lib: add lib for browsers

---
+++ src/lib/browsers.js
@@ -0,0 +1,17 @@
+const childProcess = require('child_process')
+
+const Promise = require('bluebird')
+
+const exec = Promise.promisify(childProcess.exec)
+
+/**
+ * @param url
+ * @description Open google chrome and navigate to url.
+ */
+function openGoogleChrome(url) {
+  return exec(`google-chrome ${url}`)
+}
+
+module.exports = {
+  openGoogleChrome
+}
