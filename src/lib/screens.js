a913d3bce3e38342fa5fc9655443d6bd6dff04f7
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-06-21 15:35:29 +0400

    lib: add lib for screens and pluggable screens operations

--- src/lib/screens.js
+++ src/lib/screens.js
@@ -1,6 +1,16 @@
+const fs = require('fs')
+
+const Promise = require('bluebird')
 const request = require('request-promise')
 
-function getAllPluggableScreens () {
+const readFile = Promise.promisify(fs.readFile)
+
+/**
+ * @private
+ * @returns {Promise<Array>}
+ * @description Get all pluggable screens.
+ */
+function _getAllPluggableScreens () {
   return request('https://www.renderforest.com/api/v1/templates/701/pluggable-screens')
     .then((pluggableScreensData) => {
       const _pluggableScreens = JSON.parse(pluggableScreensData).data
@@ -15,6 +25,29 @@ function getAllPluggableScreens () {
     })
 }
 
+/**
+ * @private
+ * @param {string} filePath
+ * @returns {Promise<Array>}
+ * @description Get screens from JSON file.
+ */
+function _getScreensFromFile (filePath) {
+  return readFile(filePath).then((data) => JSON.parse(data))
+}
+
+/**
+ * @returns {Promise<Object>}
+ * @description Get screens and pluggable screens.
+ */
+function getScreensAndPluggableScreens() {
+  let projectScreens
+
+  return _getScreensFromFile(process.env.JSON_FILE_PATH)
+    .then((_projectScreens) => projectScreens = _projectScreens)
+    .then(_getAllPluggableScreens)
+    .then((pluggableScreens) => ({ pluggableScreens, projectScreens }))
+}
+
 module.exports = {
-  getAllPluggableScreens
+  getScreensAndPluggableScreens
 }
