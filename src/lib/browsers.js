d480866fac0ff627fb7cc431c23cca898535bee9
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-06-21 16:11:06 +0400

    lib: separate browser opening commands for Mac and Linux

--- src/lib/browsers.js
+++ src/lib/browsers.js
@@ -1,15 +1,43 @@
 const childProcess = require('child_process')
+const os = require('os')
 
 const Promise = require('bluebird')
 
 const exec = Promise.promisify(childProcess.exec)
 
 /**
- * @param url
- * @description Open google chrome and navigate to url.
+ * @private
+ * @param {string} url
+ * @description Open Google Chrome on Mac.
+ */
+function _openGoogleChromeOnMac(url){
+  return exec(`open -a "Google Chrome" ${url}`)
+}
+
+/**
+ * @private
+ * @param {string} url
+ * @description Open Google Chrome on Linux.
+ */
+function _openGoogleChromeOnLinux(url){
+  return exec(`google-chrome ${url}`)
+}
+
+/**
+ * @param {string} url
+ * @description Open Google Chrome and navigate to the URL.
  */
 function openGoogleChrome (url) {
-  return exec(`google-chrome ${url}`)
+  const osPlatform = os.platform()
+
+  if(osPlatform.indexOf('linux') !== -1){
+    return _openGoogleChromeOnLinux(url)
+  } else if (osPlatform.indexOf('darwin') !== -1) {
+    return _openGoogleChromeOnMac(url)
+  } else {
+    console.log('Could not open Google Chrome on your OS.')
+    console.log(`\nClick on this URL to view the project: ${url}`)
+  }
 }
 
 module.exports = {
