34cbded8887e48ed698d5ef82d2d0d7f05ce25df
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-06-21 13:27:46 +0400

    src: add getScreensAndPluggableScreens(), change createProject()

--- src/index.js
+++ src/index.js
@@ -6,17 +6,39 @@ const readFile = Promise.promisify(fs.readFile)
 const exec = Promise.promisify(childProcess.exec)
 
 const projectManager = require('./project-manager')
+const pluggableScreensManager = require('./pluggable-screens-manager')
 
-function createProject() {
-  let screens
 
-  return readFile(process.env.FILE_PATH)
-    .then((data) => screens = JSON.parse(data))
-    .then(projectManager.createProject)
-    .then((responseData) => projectManager.updateProject(responseData, screens))
+/**
+ * @param {Array} payload.projectScreens
+ * @param {Array} payload.pluggableScreens
+ * @returns {Promise<Object>}
+ * @description Create project.
+ */
+function createProject(payload) {
+  const { projectScreens, pluggableScreens } = payload
+
+  return projectManager.createProject()
+    .then((responseData) => {
+      return projectManager.updateProject({
+        projectId: responseData.projectId,
+        projectScreens,
+        pluggableScreens
+      })
+    })
 }
 
-createProject()
+function getScreensAndPluggableScreens() {
+  let projectScreens
+
+  return readFile(process.env.FILE_PATH)
+    .then((data) => projectScreens = JSON.parse(data))
+    .then(() => pluggableScreensManager.getAllPluggableScreens())
+    .then((pluggableScreens) => ({ pluggableScreens, projectScreens }))
+}
+
+getScreensAndPluggableScreens()
+  .then(createProject)
   .then((data) => `https://www.renderforest.com/project/${data.projectId}`)
   .then((url) => exec(`google-chrome ${url}`))
   .catch(console.error)
