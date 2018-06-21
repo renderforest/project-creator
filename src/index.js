436ee0010120074ba40c1dee806f5c51dcb9217b
Anahit Vardanyan <vardanian.anahit@gmail.com>
2018-06-21 15:47:20 +0400

    src: use libs functions in src/index

--- src/index.js
+++ src/index.js
@@ -1,44 +1,14 @@
-const childProcess = require('child_process')
-const fs = require('fs')
+const BrowsersLib = require('./lib/browsers')
+const ProjectsLib = require('./lib/projects')
+const ScreensLib = require('./lib/screens')
 
-const Promise = require('bluebird')
-const readFile = Promise.promisify(fs.readFile)
-const exec = Promise.promisify(childProcess.exec)
+ScreensLib.getScreensAndPluggableScreens()
+  .then(ProjectsLib.createProject)
+  .then((responseData) => {
+    const url = `https://www.renderforest.com/project/${responseData.projectId}`
+    console.log(`\nFollow this URL to view the project: ${url}`)
 
-const projectManager = require('./project-manager')
-const pluggableScreensManager = require('./pluggable-screens-manager')
-
-
-/**
- * @param {Array} payload.projectScreens
- * @param {Array} payload.pluggableScreens
- * @returns {Promise<Object>}
- * @description Create project.
- */
-function createProject(payload) {
-  const { projectScreens, pluggableScreens } = payload
-
-  return projectManager.createProject()
-    .then((responseData) => {
-      return projectManager.updateProject({
-        projectId: responseData.projectId,
-        projectScreens,
-        pluggableScreens
-      })
-    })
-}
-
-function getScreensAndPluggableScreens() {
-  let projectScreens
-
-  return readFile(process.env.FILE_PATH)
-    .then((data) => projectScreens = JSON.parse(data))
-    .then(() => pluggableScreensManager.getAllPluggableScreens())
-    .then((pluggableScreens) => ({ pluggableScreens, projectScreens }))
-}
-
-getScreensAndPluggableScreens()
-  .then(createProject)
-  .then((data) => `https://www.renderforest.com/project/${data.projectId}`)
-  .then((url) => exec(`google-chrome ${url}`))
+    return url
+  })
+  .then(BrowsersLib.openGoogleChrome)
   .catch(console.error)
